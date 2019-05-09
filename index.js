c6f0813230e6ff5a6afb536a2e2e3a5012014b26
coryo12345 <corydio12@gmail.com>
2019-05-09 19:33:16 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,61 @@
+var auth = require('./auth.json');
+const Discord = require('discord.js');
+const ytdl = require('ytdl-core-discord');
+
+// Create an instance of a Discord client
+const client = new Discord.Client();
+
+/**
+ * The ready event is vital, it means that only _after_ this will your bot start reacting to information
+ * received from Discord
+ */
+client.on('ready', () => {
+    console.log('I am ready!');
+    console.log(`ID: ${client.user.username}`);
+});
+
+// Create an event listener for messages
+client.on('message', async message => {
+    // break if sent by a bot
+    if (message.author.bot) return;
+    if (message.content === '!ping') {
+        message.channel.send('pong');
+    }
+    else if (message.content.trim().substr(0,5) === '!play') {
+        let msg = message.content.trim().split(" ");
+        let url = "https://www.youtube.com/watch?v=8GW6sLrK40k";
+        try{
+            if(msg[1].length > 0){
+                url = msg[1];
+            }
+        } catch(err){
+            
+        }
+
+
+        const channel = message.member.voiceChannel;
+
+        channel.join()
+            .then(connection => {play(connection, url)})
+            .catch(console.log("error on join"));
+    }
+    else if (message.content === '!leave') {
+        const channel = message.member.voiceChannel;
+
+        channel.leave()
+            .then(connection => console.log(`Disconnected from ${channel.name}`))
+            .catch(console.log("error on leave"));
+    }
+
+
+    else if (message.content === '!help') {
+        message.channel.send("```Available commands:\n!Ping - pong\n!play [youtube link] - plays the given link\n!leave - the bot will leave YOUR channel```");
+    }
+});
+
+// Log our bot in using the token from https://discordapp.com/developers/applications/me
+client.login(auth.token);
+
+async function play(connection, url) {
+    connection.playOpusStream(await ytdl(url));
+  }
