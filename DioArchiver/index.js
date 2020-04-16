1f88cb7d2532db0219fb2985cc0705c52897f17e
coryo12345 <corydio12@gmail.com>
2020-04-15 22:12:26 -0400

    uploading zip to channel

--- DioArchiver/index.js
+++ DioArchiver/index.js
@@ -1,36 +1,21 @@
 const Discord = require('discord.js');
 const token = process.env.DISCORD_BOT_SECRET;
-
-// Create an instance of a Discord client
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
+var archiver = require('archiver');
 const client = new Discord.Client();
 
-class Proof {
-    constructor(server, channel, key) {
-        this.server = server;
-        this.channel = channel;
-        this.key = key;
-        this.answered = false;
-    }
+const template_end = "</body></html>";
+const template_open_tag = "<p>";
+const template_close_tag = "</p>";
+const template_name_open_tag = "<span>";
+const template_name_close_tag = "</span>";
 
-    prove(num) {
-        if (num.toString() === this.key.toString() && this.answered === false) {
-            this.answered = true;
-            return true;
-        }
-        return false;
-    }
-
-}
-var proofs = []
-
-/**
- * The ready event is vital, it means that only _after_ this will your bot start reacting to information
- * received from Discord
- */
 client.on('ready', () => {
     console.log('I am ready!');
     console.log(`ID: ${client.user.username}`);
-    client.user.setActivity("rip DioBot");
+    client.user.setActivity("are you there diobot?");
 });
 
 // Create an event listener for messages
@@ -40,64 +25,124 @@ client.on('message', async message => {
         if (message.author.bot) return;
 
         // archive
-        if (message.content === '!archive') {
-            try {
-                var server = message.channel.guild.name;
-            } catch (err) {
-                var server = message.author.username; 
-            }
-            let channel = message.channel.name;
-            let user = message.author.username;
-            let key = Math.round(Math.random() * (999999));
-            var p = new Proof(server, channel, key);
-            proofs.push(p);
-            console.log(`${new Date()}: Archive attempt:\n${server}\n${channel}\n${user}\nkey:${key}`);
-            message.channel.send('To confirm, type !authenticate and the key provided in the console.');
+        if (message.content.substring(0, 8) === '!archive') {
+            archive(message);
         }
-        else if (message.content.length > 14 && message.content.substr(0, 14) === '!authenticate ') {
-            try {
-                var server = message.channel.guild.name;
-            } catch (err) {
-                var server = message.author.username; 
-            }
-            let channel = message.channel.name;
-            let user = message.author.username;
-            let key = message.content.substr(14);
-            console.log(`${new Date()}: Prove attempt:\n${server}\n${channel}\n${user}\nkey:${key}`);
-            let pro = null;
-            proofs.forEach(p => {
-                if (p.server == server && p.channel == channel) {
-                    pro = p;
-                    return;
-                }
-            });
 
-            if (pro === null) {
-                return;
-            }
-
-            if (pro.prove(key)) {
-                archive(pro, message.channel);
-            }
-            else {
-                message.channel.send('Sorry - Incorrect.');
-                return;
-            }
-        }
     } catch (err) {
         console.log(err);
     }
 });
 
-function archive(pro, channel) {
-    console.log('archive channel');
-    // remove proof
-    let index = proofs.indexOf(pro);
-    if (index !== -1) proofs.splice(index, 1);
-    const filter = m => m.content.length > 0;
-    const collector = channel.createMessageCollector(filter, { max: 10, time: 15000 });
-    collector.on('collect', m => {console.log(m.content)});
-    collector.on('end', collected => {console.log('write out to file here.....')});
+client.login(token);
+
+async function archive(message) {
+    const author_id = message.author.id.toString();
+    var channel = message.channel;
+    const count = parseInt(message.content.split(' ')[1], 10);
+    if (isNaN(count)) {
+        message.channel.send("Can't parse that message.");
+        return;
+    }
+    // get approved users
+    var users = require('./users.json');
+
+    // validate a good user
+    let good = false;
+    users.approved.forEach(u => {
+        if (author_id === u.id) {
+            good = true;
+        }
+    });
+    if (!good) {
+        channel.send("Sorry. You're not an approved user.");
+        return;
+    }
+
+    // filename
+    const filename = `${channel.name}_${channel.id}.html`;
+
+    // tempdir
+    var dir_path = os.tmpdir();
+    var tmp_file_path = path.resolve(dir_path, filename);
+    console.log(tmp_file_path)
+
+    // copy meesages.html
+    fs.copyFile(path.resolve('./template.html'), tmp_file_path, err => {
+        if (err) console.error(err);
+        readMessages(channel, tmp_file_path, filename, count);
+    });
 }
 
-client.login(token);
+async function readMessages(channel, tmp_file_path, filename, count) {
+    var stream = fs.createWriteStream(tmp_file_path, { flags: 'a' });
+    // write channel name and date
+    stream.write(`<h1>${channel.name}</h1><h2>${new Date()}</h2>`);
+
+    console.log(Math.min(count, 10000));
+    // for each message -> format and write to copy
+    const messages = await messages_getter(channel, Math.min(count, 10000));
+    messages.forEach(message => {
+        message.author.fetch()
+            .then(user => {
+                const text = `${template_open_tag}${template_name_open_tag}${user.username}${template_name_close_tag}${message.cleanContent}${template_close_tag}\n`;
+                stream.write(text);
+            })
+    });
+
+    // zip up
+    zip_path = tmp_file_path.replace('.html', '.zip');
+    var output = fs.createWriteStream(zip_path);
+    var archive = archiver('zip', {
+        // gzip: true,
+        zlib: { level: 9 }
+    });
+    archive.on('error', function (err) {
+        throw err;
+    });
+    archive.pipe(output);
+    archive.file(tmp_file_path, { name: 'messages.html' });
+    await archive.finalize();
+
+    // send to channel
+    channel.send({
+        files: [{
+            attachment: zip_path,
+            name: 'messages.zip'
+        }]
+    })
+        .then(() => {
+            // clean up
+            fs.unlink(tmp_file_path, (err) => {
+                if (err) console.error(err);
+            });
+            fs.unlink(zip_path, (err) => {
+                if (err) console.error(err);
+            })
+        })
+}
+
+async function messages_getter(channel, limit) {
+    const sum_messages = [];
+    let last_id;
+
+    while (true) {
+        const options = { limit: 100 };
+        if (last_id) {
+            options.before = last_id;
+        }
+
+        const messages = await channel.messages.fetch(options);
+        sum_messages.push(...messages.array());
+        last_id = messages.last().id;
+
+        if (messages.size != 100 || sum_messages.length >= limit) {
+            break;
+        }
+    }
+
+    // if (sum_messages.length >= limit) {
+    //     sum_messages = sum_messages.slice(0, limit-1);
+    // }
+    return sum_messages;
+}
